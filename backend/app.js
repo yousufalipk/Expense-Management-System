336bee2e0a1501f050d2dbf85a0cf8582fa228e2
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-08-20 02:55:15 +0500

    Cors allowed orign changes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
@@ -9,23 +8,27 @@ const expense = require('./Routes/expenseRoutes');
 
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN;
 
-
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+    FRONTEND_ORIGIN,
+];
 
 const corsOptions = {
-    origin: FRONTEND_ORIGIN,
-    credentials: true, // Allow cookies and credentials
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 };
 
-
-// Apply CORS middleware
 app.use(cors(corsOptions));
 
-
-// Use '/api/v1/' for user routes
+// Use '/api/v1' for user routes
 app.use('/api/v1', user);
 // Use '/api/v1/expense' for expense routes
 app.use('/api/v1/expense', expense);
@@ -35,5 +38,10 @@ app.get('/', (req, res) => {
     res.send('Server Runs Correctly');
 });
 
+// Error handling middleware (optional but recommended)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 module.exports = app;
